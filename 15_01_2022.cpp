6b76ee2dca10ed4c87c5af6ecc4e8687338b7ab8
apoorv37me <85622579+apoorv37me@users.noreply.github.com>
2022-01-15 14:46:50 +0530

    Create 15_01_2022.cpp

---
+++ 15_01_2022.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define pii pair<ll, ll>
+#define vi vector < ll >
+
+#define ndl "\n"
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define F first
+#define S second
+#define T third
+#define all(a) (a).begin(), (a).end()
+#define scan(arr, n)   for (ll INPUT = 0; INPUT < n; INPUT++) cin >> arr[INPUT];
+
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7;
+const ll MAX = 1000005; // 100100
+/*-------------------------------------------------------------------*/
+
+void solve(){
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ll t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
